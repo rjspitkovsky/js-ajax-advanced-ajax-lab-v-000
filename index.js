6216677e58351531a3e83eafa948057c47f96392
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-12 21:55:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function getRepositories() {
 
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>').join('')}</ul>`
+  const src = document.getElementById("repository-template").innerHTML
+  const template = Handlebars.compile(src)
+  const repoList = template(repos)
   document.getElementById("repositories").innerHTML = repoList
 }
